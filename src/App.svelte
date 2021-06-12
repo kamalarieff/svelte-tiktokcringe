f9a07431a7ece00fec1abd7147d6c50eb502454e
Kamal Arieff <kamal.arieff@mudah.my>
2021-06-12 14:04:43 +0800

    add loading animation

--- src/App.svelte
+++ src/App.svelte
@@ -13,9 +13,7 @@
     import Post from './Post.svelte';
     let startDate = DateTime.now().minus({ months: 2 }).toISODate();
     let endDate = DateTime.now().minus({ months: 1 }).toISODate();
-    let after;
-    let promise;
-    let lastPostId;
+    let after, promise, lastPostId;
 
     $: {
         // this function will close over the new after, startDate and endDate
@@ -62,7 +60,9 @@
     <div class="flex flex-col space-y-2">
         {#await promise}
             <!-- promise is pending -->
-            <p>Loading...</p>
+            <div class="flex relative justify-center items-center h-[175px]">
+                <div class="ring-of-dots"/>
+            </div>
         {:then posts}
             <!-- promise was fulfilled -->
             {#if posts.length === 0}
@@ -114,4 +114,74 @@
   input[type="date"] {
       @apply text-black;
   }
+
+  :root {
+      --loading-half-color: #919191;
+      --loading-full-color: #5e5e5e;
+  }
+
+  /**
+  * Ring of dots
+  *
+  * @author jh3y
+*/
+  @-webkit-keyframes ring-of-dots {
+  0% {
+    -webkit-box-shadow: 20px 0px 0 0 white, 12.4698px 15.63663px 0 0 white, -4.45042px 19.49856px 0 0 white, -18.01938px 8.67767px 0 0 white, -18.01938px -8.67767px 0 0 white, -4.45042px -19.49856px 0 0 var(--loading-half-color), 12.4698px -15.63663px 0 0 var(--loading-full-color);
+            box-shadow: 20px 0px 0 0 white, 12.4698px 15.63663px 0 0 white, -4.45042px 19.49856px 0 0 white, -18.01938px 8.67767px 0 0 white, -18.01938px -8.67767px 0 0 white, -4.45042px -19.49856px 0 0 var(--loading-half-color), 12.4698px -15.63663px 0 0 var(--loading-full-color); }
+  14.28571% {
+    -webkit-box-shadow: 20px 0px 0 0 white, 12.4698px 15.63663px 0 0 white, -4.45042px 19.49856px 0 0 white, -18.01938px 8.67767px 0 0 white, -18.01938px -8.67767px 0 0 var(--loading-half-color), -4.45042px -19.49856px 0 0 var(--loading-full-color), 12.4698px -15.63663px 0 0 white;
+            box-shadow: 20px 0px 0 0 white, 12.4698px 15.63663px 0 0 white, -4.45042px 19.49856px 0 0 white, -18.01938px 8.67767px 0 0 white, -18.01938px -8.67767px 0 0 var(--loading-half-color), -4.45042px -19.49856px 0 0 var(--loading-full-color), 12.4698px -15.63663px 0 0 white; }
+  28.57143% {
+    -webkit-box-shadow: 20px 0px 0 0 white, 12.4698px 15.63663px 0 0 white, -4.45042px 19.49856px 0 0 white, -18.01938px 8.67767px 0 0 var(--loading-half-color), -18.01938px -8.67767px 0 0 var(--loading-full-color), -4.45042px -19.49856px 0 0 white, 12.4698px -15.63663px 0 0 white;
+            box-shadow: 20px 0px 0 0 white, 12.4698px 15.63663px 0 0 white, -4.45042px 19.49856px 0 0 white, -18.01938px 8.67767px 0 0 var(--loading-half-color), -18.01938px -8.67767px 0 0 var(--loading-full-color), -4.45042px -19.49856px 0 0 white, 12.4698px -15.63663px 0 0 white; }
+  42.85714% {
+    -webkit-box-shadow: 20px 0px 0 0 white, 12.4698px 15.63663px 0 0 white, -4.45042px 19.49856px 0 0 var(--loading-half-color), -18.01938px 8.67767px 0 0 var(--loading-full-color), -18.01938px -8.67767px 0 0 white, -4.45042px -19.49856px 0 0 white, 12.4698px -15.63663px 0 0 white;
+            box-shadow: 20px 0px 0 0 white, 12.4698px 15.63663px 0 0 white, -4.45042px 19.49856px 0 0 var(--loading-half-color), -18.01938px 8.67767px 0 0 var(--loading-full-color), -18.01938px -8.67767px 0 0 white, -4.45042px -19.49856px 0 0 white, 12.4698px -15.63663px 0 0 white; }
+  57.14286% {
+    -webkit-box-shadow: 20px 0px 0 0 white, 12.4698px 15.63663px 0 0 var(--loading-half-color), -4.45042px 19.49856px 0 0 var(--loading-full-color), -18.01938px 8.67767px 0 0 white, -18.01938px -8.67767px 0 0 white, -4.45042px -19.49856px 0 0 white, 12.4698px -15.63663px 0 0 white;
+            box-shadow: 20px 0px 0 0 white, 12.4698px 15.63663px 0 0 var(--loading-half-color), -4.45042px 19.49856px 0 0 var(--loading-full-color), -18.01938px 8.67767px 0 0 white, -18.01938px -8.67767px 0 0 white, -4.45042px -19.49856px 0 0 white, 12.4698px -15.63663px 0 0 white; }
+  71.42857% {
+    -webkit-box-shadow: 20px 0px 0 0 var(--loading-half-color), 12.4698px 15.63663px 0 0 var(--loading-full-color), -4.45042px 19.49856px 0 0 white, -18.01938px 8.67767px 0 0 white, -18.01938px -8.67767px 0 0 white, -4.45042px -19.49856px 0 0 white, 12.4698px -15.63663px 0 0 white;
+            box-shadow: 20px 0px 0 0 var(--loading-half-color), 12.4698px 15.63663px 0 0 var(--loading-full-color), -4.45042px 19.49856px 0 0 white, -18.01938px 8.67767px 0 0 white, -18.01938px -8.67767px 0 0 white, -4.45042px -19.49856px 0 0 white, 12.4698px -15.63663px 0 0 white; }
+  85.71429% {
+    -webkit-box-shadow: 20px 0px 0 0 var(--loading-full-color), 12.4698px 15.63663px 0 0 white, -4.45042px 19.49856px 0 0 white, -18.01938px 8.67767px 0 0 white, -18.01938px -8.67767px 0 0 white, -4.45042px -19.49856px 0 0 white, 12.4698px -15.63663px 0 0 var(--loading-half-color);
+            box-shadow: 20px 0px 0 0 var(--loading-full-color), 12.4698px 15.63663px 0 0 white, -4.45042px 19.49856px 0 0 white, -18.01938px 8.67767px 0 0 white, -18.01938px -8.67767px 0 0 white, -4.45042px -19.49856px 0 0 white, 12.4698px -15.63663px 0 0 var(--loading-half-color); }
+  100% {
+    -webkit-box-shadow: 20px 0px 0 0 white, 12.4698px 15.63663px 0 0 white, -4.45042px 19.49856px 0 0 white, -18.01938px 8.67767px 0 0 white, -18.01938px -8.67767px 0 0 white, -4.45042px -19.49856px 0 0 var(--loading-half-color), 12.4698px -15.63663px 0 0 var(--loading-full-color);
+            box-shadow: 20px 0px 0 0 white, 12.4698px 15.63663px 0 0 white, -4.45042px 19.49856px 0 0 white, -18.01938px 8.67767px 0 0 white, -18.01938px -8.67767px 0 0 white, -4.45042px -19.49856px 0 0 var(--loading-half-color), 12.4698px -15.63663px 0 0 var(--loading-full-color); } }
+@keyframes ring-of-dots {
+  0% {
+    -webkit-box-shadow: 20px 0px 0 0 white, 12.4698px 15.63663px 0 0 white, -4.45042px 19.49856px 0 0 white, -18.01938px 8.67767px 0 0 white, -18.01938px -8.67767px 0 0 white, -4.45042px -19.49856px 0 0 var(--loading-half-color), 12.4698px -15.63663px 0 0 var(--loading-full-color);
+            box-shadow: 20px 0px 0 0 white, 12.4698px 15.63663px 0 0 white, -4.45042px 19.49856px 0 0 white, -18.01938px 8.67767px 0 0 white, -18.01938px -8.67767px 0 0 white, -4.45042px -19.49856px 0 0 var(--loading-half-color), 12.4698px -15.63663px 0 0 var(--loading-full-color); }
+  14.28571% {
+    -webkit-box-shadow: 20px 0px 0 0 white, 12.4698px 15.63663px 0 0 white, -4.45042px 19.49856px 0 0 white, -18.01938px 8.67767px 0 0 white, -18.01938px -8.67767px 0 0 var(--loading-half-color), -4.45042px -19.49856px 0 0 var(--loading-full-color), 12.4698px -15.63663px 0 0 white;
+            box-shadow: 20px 0px 0 0 white, 12.4698px 15.63663px 0 0 white, -4.45042px 19.49856px 0 0 white, -18.01938px 8.67767px 0 0 white, -18.01938px -8.67767px 0 0 var(--loading-half-color), -4.45042px -19.49856px 0 0 var(--loading-full-color), 12.4698px -15.63663px 0 0 white; }
+  28.57143% {
+    -webkit-box-shadow: 20px 0px 0 0 white, 12.4698px 15.63663px 0 0 white, -4.45042px 19.49856px 0 0 white, -18.01938px 8.67767px 0 0 var(--loading-half-color), -18.01938px -8.67767px 0 0 var(--loading-full-color), -4.45042px -19.49856px 0 0 white, 12.4698px -15.63663px 0 0 white;
+            box-shadow: 20px 0px 0 0 white, 12.4698px 15.63663px 0 0 white, -4.45042px 19.49856px 0 0 white, -18.01938px 8.67767px 0 0 var(--loading-half-color), -18.01938px -8.67767px 0 0 var(--loading-full-color), -4.45042px -19.49856px 0 0 white, 12.4698px -15.63663px 0 0 white; }
+  42.85714% {
+    -webkit-box-shadow: 20px 0px 0 0 white, 12.4698px 15.63663px 0 0 white, -4.45042px 19.49856px 0 0 var(--loading-half-color), -18.01938px 8.67767px 0 0 var(--loading-full-color), -18.01938px -8.67767px 0 0 white, -4.45042px -19.49856px 0 0 white, 12.4698px -15.63663px 0 0 white;
+            box-shadow: 20px 0px 0 0 white, 12.4698px 15.63663px 0 0 white, -4.45042px 19.49856px 0 0 var(--loading-half-color), -18.01938px 8.67767px 0 0 var(--loading-full-color), -18.01938px -8.67767px 0 0 white, -4.45042px -19.49856px 0 0 white, 12.4698px -15.63663px 0 0 white; }
+  57.14286% {
+    -webkit-box-shadow: 20px 0px 0 0 white, 12.4698px 15.63663px 0 0 var(--loading-half-color), -4.45042px 19.49856px 0 0 var(--loading-full-color), -18.01938px 8.67767px 0 0 white, -18.01938px -8.67767px 0 0 white, -4.45042px -19.49856px 0 0 white, 12.4698px -15.63663px 0 0 white;
+            box-shadow: 20px 0px 0 0 white, 12.4698px 15.63663px 0 0 var(--loading-half-color), -4.45042px 19.49856px 0 0 var(--loading-full-color), -18.01938px 8.67767px 0 0 white, -18.01938px -8.67767px 0 0 white, -4.45042px -19.49856px 0 0 white, 12.4698px -15.63663px 0 0 white; }
+  71.42857% {
+    -webkit-box-shadow: 20px 0px 0 0 var(--loading-half-color), 12.4698px 15.63663px 0 0 var(--loading-full-color), -4.45042px 19.49856px 0 0 white, -18.01938px 8.67767px 0 0 white, -18.01938px -8.67767px 0 0 white, -4.45042px -19.49856px 0 0 white, 12.4698px -15.63663px 0 0 white;
+            box-shadow: 20px 0px 0 0 var(--loading-half-color), 12.4698px 15.63663px 0 0 var(--loading-full-color), -4.45042px 19.49856px 0 0 white, -18.01938px 8.67767px 0 0 white, -18.01938px -8.67767px 0 0 white, -4.45042px -19.49856px 0 0 white, 12.4698px -15.63663px 0 0 white; }
+  85.71429% {
+    -webkit-box-shadow: 20px 0px 0 0 var(--loading-full-color), 12.4698px 15.63663px 0 0 white, -4.45042px 19.49856px 0 0 white, -18.01938px 8.67767px 0 0 white, -18.01938px -8.67767px 0 0 white, -4.45042px -19.49856px 0 0 white, 12.4698px -15.63663px 0 0 var(--loading-half-color);
+            box-shadow: 20px 0px 0 0 var(--loading-full-color), 12.4698px 15.63663px 0 0 white, -4.45042px 19.49856px 0 0 white, -18.01938px 8.67767px 0 0 white, -18.01938px -8.67767px 0 0 white, -4.45042px -19.49856px 0 0 white, 12.4698px -15.63663px 0 0 var(--loading-half-color); }
+  100% {
+    -webkit-box-shadow: 20px 0px 0 0 white, 12.4698px 15.63663px 0 0 white, -4.45042px 19.49856px 0 0 white, -18.01938px 8.67767px 0 0 white, -18.01938px -8.67767px 0 0 white, -4.45042px -19.49856px 0 0 var(--loading-half-color), 12.4698px -15.63663px 0 0 var(--loading-full-color);
+            box-shadow: 20px 0px 0 0 white, 12.4698px 15.63663px 0 0 white, -4.45042px 19.49856px 0 0 white, -18.01938px 8.67767px 0 0 white, -18.01938px -8.67767px 0 0 white, -4.45042px -19.49856px 0 0 var(--loading-half-color), 12.4698px -15.63663px 0 0 var(--loading-full-color); } }
+
+
+.ring-of-dots:before {
+  animation: ring-of-dots 1s infinite linear reverse;
+  border-radius: 10px;
+  content: '';
+  display: block;
+  height: 10px;
+  width: 10px; }
 </style>
