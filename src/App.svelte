c0d7bfde75f6f2eefcdd51565ff49420e6fcdef7
Kamal Arieff <kamal.arieff@mudah.my>
2021-06-12 12:15:56 +0800

    change reactivity

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,11 @@
     import axios from 'axios';
     import { DateTime } from 'luxon';
 	export let name;
-    let startDate = DateTime.now().toISODate();
-    let endDate = DateTime.now().toISODate();
+    let startDate = DateTime.now().minus({ months: 2 }).toISODate();
+    let endDate = DateTime.now().minus({ months: 1 }).toISODate();
     let after;
+    let promise;
+    let lastPostId;
     
     const tagBackgroundColors = {
         'Humor/Cringe': '#7c5295',
@@ -18,33 +20,32 @@
         'Politics': 'rgb(160, 99, 36)',
         'Discussion': 'rgb(13, 211, 187)'
     };
-    
-    async function getRedditPosts(after = '') {
-        let res = await axios.get(`https://www.reddit.com/r/TikTokCringe/top.json?t=year&limit=100${after !== '' ? `&after=t3_${after}` : ''}`);
-        const start = DateTime.fromISO(startDate).toSeconds();
-        const end = DateTime.fromISO(endDate).toSeconds();
-        res = res.data.data.children.filter(post => {
-            const { created } = post.data;
-            return created > start && created < end;
-        })
-        res = res.filter(post => {
-            const { link_flair_text } = post.data;
-            return !["Politics"].includes(link_flair_text);
-        })
-        return res;
+
+    $: {
+        // this function will close over the new after, startDate and endDate
+        async function getRedditPosts() {
+            let { data: { data: { children: res } } } = await axios.get(`https://www.reddit.com/r/TikTokCringe/top.json?t=year&limit=100${after !== undefined ? `&after=t3_${after}` : ''}`);
+            const start = DateTime.fromISO(startDate).toSeconds();
+            const end = DateTime.fromISO(endDate).toSeconds();
+            res = res.filter(post => {
+                const { created } = post.data;
+                return created > start && created < end;
+            })
+            res = res.filter(post => {
+                const { link_flair_text } = post.data;
+                return !["Politics"].includes(link_flair_text);
+            })
+            lastPostId = res[res.length - 1]?.data.id ?? undefined;
+            return res;
+        }
+        
+        promise = getRedditPosts();
     }
-
-
-    $: promise = getRedditPosts(after, startDate, endDate);
-    $: lastPostId = promise.then(res => res[res.length - 1].data.id);
 
     const handleClick = () => {
-        console.log('lastPostId', lastPostId);
-        const temp = lastPostId.then(res => {
-            console.log('res', res);
-            after = res;
-        });
+        after = lastPostId;
     }
+
 </script>
 
 <main class="max-w-3xl mx-auto space-y-2 py-4">
@@ -64,43 +65,49 @@
             <p>Loading...</p>
         {:then posts}
             <!-- promise was fulfilled -->
-            {#each posts as {
-                data: {
-                    title,
-                    thumbnail,
-                    thumbnail_width,
-                    thumbnail_height,
-                    permalink,
-                    full_link,
-                    score,
-                    url,
-                    created,
-                    id,
-                    link_flair_text,
-                    media 
+            {#if posts.length === 0}
+                No more posts.
+            {:else}
+                {#each posts as {
+                        data: {
+                            title,
+                            thumbnail,
+                            thumbnail_width,
+                            thumbnail_height,
+                            permalink,
+                            full_link,
+                            score,
+                            url,
+                            created,
+                            id,
+                            link_flair_text,
+                            media,
+                            preview
+                        }
+                    }
                 }
-            }}
-            <div class="grid grid-cols-3 grid-gap-2 rounded-md mb-4">
-                <div class="col-span-2">
-                    <video height="488px" preload="auto" class="max-h-[488px] !w-full" controls>
-                        <source src={media?.reddit_video?.fallback_url} type="video/mp4">
-                    </video>
-                </div>
-                <div class="flex flex-col space-y-2 border border-gray-200 p-2">
-                    <span class="rounded-full p-2 w-[fit-content] text-white font-medium" style={`background-color: ${tagBackgroundColors[link_flair_text]}`}>{link_flair_text}</span>
-                    <a href={`https://reddit.com${permalink}`} target="_blank">
-                        <!--
-                            <img src={thumbnail} onerror="if (this.src != 'error.jpg') this.src = '/images/not-found.svg';" class='!h-[488px] !w-[488px]'/>
-                        -->
-                        <div class="flex flex-col space-y-2">
-                            <span><span class="font-bold">{id}</span>: {title}</span>
-                            <span class="font-medium text-blue-500">{score}</span>
-                            <time datetime={DateTime.fromSeconds(created).toISODate()}>{DateTime.fromSeconds(created).toISODate()}</time>
+                    <div class="grid grid-cols-3 grid-gap-2 rounded-md mb-4">
+                        <div class="col-span-2">
+                            <video height="488px" class="max-h-[488px] !w-full" controls poster={preview?.images[0]?.resolutions[2]?.url.replaceAll('amp;', '')}>
+                                <source src={media?.reddit_video?.fallback_url} type="video/mp4">
+                            </video>
                         </div>
-                    </a>
-                </div>
-            </div>
-            {/each}
+                        <div class="flex flex-col space-y-2 border border-gray-200 p-2">
+                            <span class="rounded-full p-2 w-[fit-content] text-white font-medium" style={`background-color: ${tagBackgroundColors[link_flair_text]}`}>{link_flair_text}</span>
+                            <a href={`https://reddit.com${permalink}`} target="_blank">
+                                <!--
+                                    <img src={thumbnail} onerror="if (this.src != 'error.jpg') this.src = '/images/not-found.svg';" class='!h-[488px] !w-[488px]'/>
+                                -->
+                                <div class="flex flex-col space-y-2">
+                                    <span><span class="font-bold">{id}</span>: {title}</span>
+                                    <span class="font-medium text-blue-500">{score}</span>
+                                    <time datetime={DateTime.fromSeconds(created).toISODate()}>{DateTime.fromSeconds(created).toISODate()}</time>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                {/each}
+            {/if}
         {:catch error}
             <!-- promise was rejected -->
             <p>Something went wrong: {error.message}</p>
