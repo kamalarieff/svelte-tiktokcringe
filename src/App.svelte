297ba81cc8ff69c010196650c171b7a866c2d9be
Kamal Arieff <kamal.arieff@mudah.my>
2021-06-12 16:00:11 +0800

    add title

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <svelte:head>
+    <title>TikTokCringe's Top Posts</title>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
     <style>
